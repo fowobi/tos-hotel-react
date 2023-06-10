a21f9c40ad1af29a2f39b3220ca1ac0f8eced1c5
fowobi <109865505+fowobi@users.noreply.github.com>
2023-06-10 13:12:32 +0100

    update on searchresult and app css

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,7 +1,68 @@
-import React from "react";
+// import React from "react";
+// import moment from "moment";
+
+// const SearchResults = ({ bookings }) => {
+//   return (
+//     <div className="search-results">
+//       <table className="table">
+//         <thead>
+//           <tr>
+//             <th>ID</th>
+//             <th>Title</th>
+//             <th>First Name</th>
+//             <th>Surname</th>
+//             <th>Email</th>
+//             <th>Room ID</th>
+//             <th>Check-In Date</th>
+//             <th>Check-Out Date</th>
+//             <th>Nights</th>
+//           </tr>
+//         </thead>
+//         <tbody>
+//           {bookings.map((booking) => {
+//             const checkInDate = moment(booking.checkInDate);
+//             const checkOutDate = moment(booking.checkOutDate);
+//             const nights = checkOutDate.diff(checkInDate, "days");
+
+//             return (
+//               <tr key={booking.id}>
+//                 <td>{booking.id}</td>
+//                 <td>{booking.title}</td>
+//                 <td>{booking.firstName}</td>
+//                 <td>{booking.surname}</td>
+//                 <td>{booking.email}</td>
+//                 <td>{booking.roomId}</td>
+//                 <td>{booking.checkInDate}</td>
+//                 <td>{booking.checkOutDate}</td>
+//                 <td>{nights}</td>
+//               </tr>
+//             );
+//           })}
+//         </tbody>
+//       </table>
+//     </div>
+//   );
+// };
+
+// export default SearchResults;
+
+
+import React, { useState } from "react";
 import moment from "moment";
 
 const SearchResults = ({ bookings }) => {
+  const [selectedRows, setSelectedRows] = useState([]);
+
+  const toggleRowSelection = (id) => {
+    setSelectedRows((prevSelectedRows) => {
+      if (prevSelectedRows.includes(id)) {
+        return prevSelectedRows.filter((rowId) => rowId !== id);
+      } else {
+        return [...prevSelectedRows, id];
+      }
+    });
+  };
+
   return (
     <div className="search-results">
       <table className="table">
@@ -25,7 +86,11 @@ const SearchResults = ({ bookings }) => {
             const nights = checkOutDate.diff(checkInDate, "days");
 
             return (
-              <tr key={booking.id}>
+              <tr
+                key={booking.id}
+                onClick={() => toggleRowSelection(booking.id)}
+                className={selectedRows.includes(booking.id) ? "selected" : ""}
+              >
                 <td>{booking.id}</td>
                 <td>{booking.title}</td>
                 <td>{booking.firstName}</td>
@@ -45,3 +110,4 @@ const SearchResults = ({ bookings }) => {
 };
 
 export default SearchResults;
+
